3da7d497c1ce4cfac703f1080fb8897ad595031a
pena1973 <penapisemnet@gmail.com>
2023-12-29 22:26:19 +0200

    ready1

--- index.ts
+++ index.ts
@@ -3,25 +3,11 @@ import * as dotenv from 'dotenv';
 
 import {BooksController} from "./src/controllers/BooksController"
 import {AuthController} from "./src/controllers/AuthController"
-// import {UserController} from "./src/controllers/UserController"
-// import { createClient } from '@supabase/supabase-js'
 
 import {BooksService} from "./src/api/BooksService"
 import {AuthService} from "./src/api/AuthService"
 // dotend получать доступ к env-переменным из файла .env
 dotenv.config();
-
-// async function bootstrap() {
-//     // разовая инициализация
-//     // каждый экземпляр  -  это одно соединение
-//     const booksService = new BooksService();
-//     const authService = new AuthService();
-// //     const bookController = new BooksController();
-// //     const userController = new UserController();
-
-//     const app = new App(booksService,authService);    
-//     await app.run();
-// }  
 
 async function bootstrap() {
     const bookController = new BooksController(new BooksService());
